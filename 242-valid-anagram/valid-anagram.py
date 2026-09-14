ea0a2511e574c9045265db29fef7530be3f31ff7
Amannnn01 <cyphervalorant148@gmail.com>
2026-09-14 15:53:24 +0530

    Time: 18 ms (64.89%) | Memory: 12.5 MB (67.29%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+
+        if len(s) != len(t):
+            return False
+
+        d1={}
+        d2={}
+        for ch in s:
+            d1[ch] = d1.get(ch , 0)+1
+
+        for ch in t:
+            d2[ch] = d2.get(ch , 0)+1
+
+        if d1==d2:
+            return True
+
+        return False
+
+        
+        
+        
+
+
+        
+
+        
+        
+
+    
+
+
+
+
+        
+        
+        
