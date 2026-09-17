fbaa433c57d9bbd7589ce497c495d350d6d7b0a8
Amannnn01 <cyphervalorant148@gmail.com>
2026-09-17 23:42:56 +0530

    Time: 7 ms (80.58%) | Memory: 13.5 MB (30.25%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def trap(self, height):
+        left=0
+        right=len(height)-1
+        water=0
+
+        left_max=0
+        right_max=0
+        while left<right:
+            if height[left] <= height[right]:
+                if height[left] > left_max:
+                    left_max=height[left]
+                
+                else:
+                    water = water + left_max - height[left]
+                
+                left +=1
+
+            else:
+                
+                if height[right] >= right_max:
+                    right_max=height[right]
+                else:
+                    water= water + right_max - height[right]
+                right=right-1
+        return water
+                    
+
+       
+        
